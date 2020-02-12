8fec9a77fd91184b21d70e8d61bc511d7aa06f32
priscilafernandes <pris1@outlook.com.br>
2020-02-12 16:48:06 -0300

    validação de email ok

---
+++ src/app.js
@@ -0,0 +1,16 @@
+let inputEmail = document.getElementById("input-email");
+const btnEmail = document.getElementById("btn-email");
+const formEmail = document.getElementById("form-email");
+
+btnEmail.addEventListener("click", submit);
+
+function submit() {
+  let valueEmail = inputEmail.value;
+  const validate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (validate.test(valueEmail)) {
+    alert("Submit!");
+  } else {
+    alert("email incorreto, tente novamente");
+  }
+}
