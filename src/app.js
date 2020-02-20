82b4cc516ea29591cdc999095152650e41dff1b4
priscilafernandes <pris1@outlook.com.br>
2020-02-20 17:31:49 -0300

    ajustes na validação de email

--- src/app.js
+++ src/app.js
@@ -1,34 +1,40 @@
+const formEmail = document.getElementById("form-email");
 const btnEmail = document.getElementById("btn-email");
 btnEmail.addEventListener("click", submit);
 
-function submit() {
-  let inputEmail = document.getElementById("input-email").value;
+function submit(e) {
+  e.preventDefault();
+
+  let inputEmail = document.getElementById("input-email");
   const validate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-  if (!validate.test(inputEmail)) {
+
+  if (!validate.test(inputEmail.value) || inputEmail.value.length <= 0) {
     errorMsg();
   } else {
     alert("Submit!");
-  }
+    formEmail.reset();
+  } 
 }
 
 function errorMsg() {
-  btnEmail.removeEventListener("click", submit);
   renderErrorIcon();
   renderErrorMsg();
+  btnEmail.removeEventListener("click", submit);
 }
 
 function renderErrorIcon() {
   let node = document.createElement("img");
   node.setAttribute("src", "./images/icon-error.svg");
   node.classList.add("error-icon");
-  document.getElementById("form-email").appendChild(node);
+  node.id = "error-icon";
+  formEmail.appendChild(node);
 }
 
 function renderErrorMsg() {
   let node = document.createElement("p");
   let msg = document.createTextNode("Please provide a valid email");  
   node.classList.add("error-msg");
+  node.id = "error-msg";
   node.appendChild(msg);  
   document.getElementById("block-email").appendChild(node);
 }
